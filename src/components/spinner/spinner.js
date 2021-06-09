e1540940000967af46e30ce30e4261a11ac2e5b6
Артем Семенов <aosemenov_1@miem.hse.ru>
2021-06-09 16:03:13 +0300

    50 / Adding component functions:

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return(
+        <div className="loadingio-spinner-double-ring-fnj9bt06m5">
+            <div className="ldio-mv6mln4vmvi">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
